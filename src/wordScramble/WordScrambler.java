ce7198924214804766e38b353c1f3d9c8005dfbc
Chris Day <montymontgomeri@gmail.com>
2019-12-02 16:15:17 -0500

    Added Names and Verbs

--- src/wordScramble/WordScrambler.java
+++ src/wordScramble/WordScrambler.java
@@ -7,33 +7,45 @@ public class WordScrambler {
 
 	private static ArrayList<String> adjectives = new ArrayList<>();
 	private static ArrayList<String> nouns = new ArrayList<>();
+	private static ArrayList<String> names = new ArrayList<>();
+	private static ArrayList<String> verbs = new ArrayList<>();
+	private static int questions = 1;
 
 	public static void main(String[] args) {
-
-		askNumber();
-	}
-	
-	public static void askNumber() {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Give me a number");
-		try {
-			String input = scanner.nextLine();
-			int num = Integer.parseInt(input);
-			repeatDisplay(num);
-		} catch (Exception e) {
-			System.out.println("Ain't you ever typed a number before? Try again!");
-			askNumber();
-		}
 		
-	}
-
-	public static void repeatDisplay(int num) {
-		for (int i = 0; i < num; i++) {
-			display();
+		while (true) {
+		display();
 		}
 	}
 
 	public static void display() {
+
+		Scanner scanner = new Scanner(System.in);
+		
+		if (questions % 4 == 0) {
+			System.out.println("Type in someone's name.");
+			String input = scanner.nextLine();
+			names.add(input.toString());
+			
+			randomSentence(randomNoun(), randomAdj());
+			System.out.println("\n");
+			questions++;
+			
+		} else if (questions % 5 == 0) {
+			System.out.println("Type in a verb.");
+			String input = scanner.nextLine();
+			verbs.add(input.toString());
+			
+			randomSentence(randomNoun(), randomAdj());
+			System.out.println("\n");
+			questions++;
+		} else
+			askAndDisplay();
+			
+	}
+	
+	public static void askAndDisplay() {
+		
 		Scanner scanner = new Scanner(System.in);
 
 		System.out.println("Type in a noun(thing)");
@@ -45,7 +57,8 @@ public class WordScrambler {
 		adjectives.add(input2.toString());
 
 		randomSentence(randomNoun(), randomAdj());
-
+		System.out.println("\n");
+		questions++;
 	}
 
 	public static int randomNumber(int limit) {
@@ -54,25 +67,40 @@ public class WordScrambler {
 	}
 
 	public static String randomNoun() {
-
 		int size = randomNumber(nouns.size());
-
 		return nouns.get(size);
 	}
 
 	public static String randomAdj() {
-
 		int size = randomNumber(adjectives.size());
-
 		return adjectives.get(size);
+	}
+	
+	public static String randomName() {
+		if (questions>=4) {
+		int size = randomNumber(names.size());
+		return names.get(size);
+		}
+		return "Frank";
+	}
+	
+	public static String randomVerb() {
+		if (questions>=5 ) {
+		int size = randomNumber(verbs.size());
+		return verbs.get(size);
+		} else
+			return "Corn Nobbled";
 	}
 
 	public static void randomSentence(String noun, String adj) {
-
+		
 		int num = randomNumber(adjectives.size());
+		
 		String nounTwo = randomNoun();
 		String adjTwo = randomAdj();
-
+		String name = randomName();
+		String verb = randomVerb();
+		
 		if (num == 0) {
 			System.out.println("The " + noun + " is very " + adj);
 		} else if (num == 1) {
@@ -86,18 +114,19 @@ public class WordScrambler {
 			System.out.println("If I had a " + noun + " for every time I heard that! Then I'd have a " + adj + " "
 					+ nounTwo + ".");
 		} else if (num == 5) {
-			System.out.println("Take a " + adj + " " + adjTwo + " " + noun + " and put it on your " + nounTwo + ".");
+			System.out.println("Take a " + adj + " " + adjTwo + " " + noun + " and put it on your " + nounTwo + " " + name + ".");
 		} else if (num == 6) {
 			System.out.println(
-					"If feared for my " + noun + "'s life. I had never seen such a " + adj + " " + nounTwo + ".");
+					"If feared for my " + noun + "'s life." + name + " had never seen such a " + adj + " " + nounTwo + " " + verb + " like that before.");
 		} else if (num == 7) {
-			System.out.println("Don't look at my " + noun + " like that!");
+			System.out.println("Don't look at my " + noun + " like that " + name + "!");
 		} else if (num == 8) {
-			System.out.println("It was a " + adj + " and " + adjTwo + " night when the " + noun + " came in.");
+			System.out.println("It was a " + adj + " and " + adjTwo + " night when the " + noun + " " + verb + " into the " + nounTwo + ".");
 		} else if (num == 9) {
-			System.out.println("Just me and my " + adj + " " + adjTwo + " " + noun + " against your big " + nounTwo + ".");
+			System.out.println(
+					"Just me and my " + adj + " " + adjTwo + " " + noun + " against your big " + nounTwo + " " + name + ". There's no telling who's gonna " + verb + " first.");
 		} else
-			System.out.println("Don't waste my " + noun + " on " + nounTwo + "s you " + adj + " dunderheads!");
+			System.out.println("Don't waste my " + noun + " on " + nounTwo + "s you " + adj + " dunderheads!" + " I'm talking to you " + name + "!");
 
 	}
 
