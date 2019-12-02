1f1dd86c14f7a1234a48fa0b656a6edda367fb22
Chris Day <montymontgomeri@gmail.com>
2019-12-02 14:47:01 -0500

    Minor fix

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # WordScrabbler
+
+Run as a Java Project. It is a simple(and silly) command-line program.
+
+First it will ask you for a number. That will be how many times it will ask you for a noun and then an adjective.
+
+Each time you type in both a noun then an adjective it then scrambles every answer you've put in and creates random sentences with the words. 
+
+Made this in my break time at school just for fun. 
--- src/wordScramble/WordScrambler.java
+++ src/wordScramble/WordScrambler.java
@@ -9,38 +9,48 @@ public class WordScrambler {
 	private static ArrayList<String> nouns = new ArrayList<>();
 
 	public static void main(String[] args) {
-		
+
+		askNumber();
+	}
+	
+	public static void askNumber() {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Give me a number");
-		String input = scanner.nextLine();
-		int num = Integer.parseInt(input);
-		repeatDisplay(num);
-	}
-	
-	public static void repeatDisplay(int num) {
-		for(int i = 0; i < num; i++) {
-		display();
+		try {
+			String input = scanner.nextLine();
+			int num = Integer.parseInt(input);
+			repeatDisplay(num);
+		} catch (Exception e) {
+			System.out.println("Ain't you ever typed a number before? Try again!");
+			askNumber();
 		}
-	}
-	
-	public static void display() {
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.println("Type in a noun");
-		String input = scanner.nextLine();
-		nouns.add(input.toString());
-		
-		System.out.println("Type in an adjective");
-		String input2 = scanner.nextLine();
-		adjectives.add(input2.toString());
-		
-		System.out.println("The " + randomNoun() + " was " + randomAdj());
 		
 	}
 
+	public static void repeatDisplay(int num) {
+		for (int i = 0; i < num; i++) {
+			display();
+		}
+	}
+
+	public static void display() {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Type in a noun");
+		String input = scanner.nextLine();
+		nouns.add(input.toString());
+
+		System.out.println("Type in an adjective");
+		String input2 = scanner.nextLine();
+		adjectives.add(input2.toString());
+
+		randomSentence(randomNoun(), randomAdj());
+
+	}
+
 	public static int randomNumber(int limit) {
-		 int randomInt = (int)(limit * Math.random());
-		 return randomInt;
+		int randomInt = (int) (limit * Math.random());
+		return randomInt;
 	}
 
 	public static String randomNoun() {
@@ -55,6 +65,33 @@ public class WordScrambler {
 		int size = randomNumber(adjectives.size());
 
 		return adjectives.get(size);
+	}
+
+	public static void randomSentence(String noun, String adj) {
+
+		int num = randomNumber(adjectives.size());
+		String nounTwo = randomNoun();
+		String adjTwo = randomAdj();
+
+		if (num == 0) {
+			System.out.println("The " + noun + " is very " + adj);
+		} else if (num == 1) {
+			System.out.println("How " + adj + " is your " + noun + " would you say?");
+		} else if (num == 2) {
+			System.out.println("I really hate " + adj + " and " + adjTwo + " " + noun + "s.");
+		} else if (num == 3) {
+			System.out.println("If you take a " + adj + " " + noun + " and add a " + nounTwo
+					+ " then you got yourself an extremely " + adjTwo + " " + noun);
+		} else if (num == 4) {
+			System.out.println("If I had a " + noun + " for every time I heard that! Then I'd have a " + adj + " "
+					+ nounTwo + ".");
+		} else if (num == 5) {
+			System.out.println("Take a " + adj + " " + adjTwo + " " + noun + " and put it on your " + nounTwo + ".");
+		} else if (num == 6) {
+			System.out.println(
+					"If feared for my " + noun + "'s life. I had never seen such a " + adj + " " + nounTwo + ".");
+		}
+
 	}
 
 }
